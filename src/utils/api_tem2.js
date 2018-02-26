593f6d500c20f2a82059becf1f93837a2db3e2dc
ZhaiXiaoyuan <2549004118@qq.com>
2018-02-27 02:43:46 +0800

    20180227

---
+++ src/utils/api_tem2.js
@@ -0,0 +1,46 @@
+/**
+ * 接口地址放在这里，存放在Vue的全局自定义方法
+ * */
+export default {
+    install: function (Vue, options) {
+
+      Vue.http.options.emulateJSON = true;
+      Vue.http.interceptors.push((request, next)  =>{
+
+        next((response) => {
+          return response
+        });
+
+      });
+        /*自定义ajax函数，自带的不好用*/
+        Vue.http.ajax = async function (options) {
+            if(options.method.toUpperCase() == 'GET'){
+                let res = await Vue.http.get(options.url, {params: options.params});
+                if(typeof res.body == 'string'){
+                    return JSON.parse(res.body);
+                }else{
+                    return res.body;
+                }
+            }else if(options.method.toUpperCase() == 'POST'){
+                let res = await Vue.http.post(options.url, options.params);
+                if(typeof res.body == 'string'){
+                    return JSON.parse(res.body);
+                }else{
+                    return res.body;
+                }
+            }
+        }
+        let basicUrl='/love/';
+        Vue.api = {
+            /*登录*/
+            loginByPhone: function (params) {
+                return Vue.http.ajax({
+                    method: 'post',
+                    url: basicUrl + 'domain/loginByPhone',
+                    params: params
+                });
+            },
+        }
+    },
+
+}
